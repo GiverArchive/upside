19657289983f5c64e9067c208272355656da8e68
GiverPlay007 <play.giver.007@gmail.com>
2021-09-20 19:45:43 -0300

    Convert TextController into a class

--- src/Router.ts
+++ src/Router.ts
@@ -1,9 +1,11 @@
 import { Request, Response, Router } from "express";
 
-import { reverse } from "./controllers/TextController";
+import { TextController } from "./controllers/TextController";
+
+const textController = new TextController();
 
 const router = Router();
 
-router.post("/text/reverse", reverse);
+router.post("/text/reverse", textController.reverse);
 
 export default router;
--- src/controllers/TextController.ts
+++ src/controllers/TextController.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from "express";
 import { AppError } from "./errors/AppError";
 
-const reverse = (req: Request, res: Response) => {
-  const { text } = req.query;
+class TextController {
+  reverse(req: Request, res: Response) {
+    const { text } = req.query;
+  
+    if(!text) throw new AppError("Missing text parameter.");
+  
+    const reversed = (text as string).split('').reverse().join('');
+  
+    return res.status(200).json({ reversed });
+  };
+}
 
-  if(!text) throw new AppError("Missing text parameter.");
-
-  const reversed = (text as string).split('').reverse().join('');
-
-  return res.status(200).json({ reversed });
-};
-
-export { reverse };
+export { TextController };
